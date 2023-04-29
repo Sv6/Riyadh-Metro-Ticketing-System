f893d129abf709ab757f16576a8f1efe22e06563
Sul <76082727+Sv6@users.noreply.github.com>
2023-04-29 17:46:37 +0300

    gp

---
+++ android/app/src/main/kotlin/com/example/riyadh_metro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riyadh_metro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/SignUp.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  // State<myApp> createState() => _myAppState();
+
+// class _myAppState extends State<myApp> {
+  bool hidePass = true;
+//   bool hideVerifyPass = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup Page',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.green,
+            ), // set the border color to green
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.green,
+            ), // set the border color to green
+          ),
+          prefixStyle: TextStyle(
+            color: Colors.white, // set the prefix icon color to white
+          ),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // ========================= LOGO =========================
+
+            //========================= SIGN UP TEXT =========================
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10.0, right: 15.0),
+                    child: Divider(
+                      color: Colors.green,
+                      height: 10,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Sign up',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10.0, right: 15.0),
+                    child: Divider(
+                      color: Colors.green,
+                      height: 10,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50.0),
+            // ========================= FULL NAME =========================
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Full Name',
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            // ========================= BIRTH DATE =========================
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Birth Date',
+                  prefixIcon: Icon(Icons.calendar_today),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            // ========================= USERNAME =========================
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            // ========================= PASSWORD =========================
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      // setState(() {
+                      //   hidePass = !hidePass;
+                      // });
+                    },
+                    child: Icon(
+                        hidePass ? Icons.visibility : Icons.visibility_off),
+                  ),
+                ),
+                obscureText: hidePass,
+              ),
+            ),
+// ========================= PASSWORD VERIFICATION =========================
+            SizedBox(height: 20.0),
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Confirm Password',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      // setState(() {
+                      //   hidePass = !hidePass;
+                      // });
+                    },
+                    child: Icon(
+                        hidePass ? Icons.visibility : Icons.visibility_off),
+                  ),
+                ),
+                obscureText: hidePass,
+              ),
+            ),
+// ========================= EMAIL =========================
+            SizedBox(height: 20.0),
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+            ),
+// ========================= PHONE NUMBER =========================
+            SizedBox(height: 20.0),
+            Container(
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Phone Number',
+                  prefixIcon: Icon(Icons.phone),
+                ),
+              ),
+            ),
+// ========================= SIGN UP BUTTON =========================
+            SizedBox(height: 50.0),
+            Container(
+              width: 300.0,
+              height: 50.0,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+// ========================= SIGN IN =========================
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Already have an account? Sign in.',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
