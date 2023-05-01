76aa73abc6084f65e60793eb98789b74efe300fb
Sul <76082727+Sv6@users.noreply.github.com>
2023-05-01 22:34:56 +0300

    gp

---
+++ lib/book.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(myApp(clientName: "clientName", balance: 0, destinations: ["destination", "jioadsfjo"], selectedDestination: "destination", availableTickets: ["d"], selectedTicket: "d"));
+}
+
+class myApp extends StatelessWidget {
+  final String clientName;
+  final double balance;
+  final List<String> destinations;
+  final String selectedDestination;
+  final List<String> availableTickets;
+  final String selectedTicket;
+
+  myApp({
+    required this.clientName,
+    required this.balance,
+    required this.destinations,
+    required this.selectedDestination,
+    required this.availableTickets,
+    required this.selectedTicket,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home Page',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome, $clientName!"),
+          backgroundColor: Color.fromARGB(255, 6, 179, 107),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Text(
+                        "Balance:",
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "\$${balance.toStringAsFixed(2)}",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "From:",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                              DropdownButton<String>(
+                                value: selectedDestination,
+                                onChanged: (String? value) {},
+                                items: destinations
+                                    .map(
+                                        (destination) => DropdownMenuItem<String>(
+                                      value: destination,
+                                      child: Text(destination),
+                                    ))
+                                    .toList(),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "To:",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                              DropdownButton<String>(
+                                value: selectedDestination,
+                                onChanged: (String? value) {},
+                                items: destinations
+                                    .map(
+                                        (destination) => DropdownMenuItem<String>(
+                                      value: destination,
+                                      child: Text(destination),
+                                    ))
+                                    .toList(),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      "Available tickets:",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    availableTickets.isEmpty
+                        ? Text(
+                      "You have no tickets.",
+                      style: TextStyle(fontSize: 16.0),
+                    )
+                        : Column(
+                      children: availableTickets
+                          .map((ticket) => InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.circular(
+                              8.0,
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(ticket),
+                              Icon(Icons.arrow_forward),
+                            ],
+                          ),
+                        ),
+                      ))
+                          .toList(),
+                    ),
+                    SizedBox(height: 16.0),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "Book now",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: "Booking",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: "Map",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/client.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+void main() {
+  runApp(myApp(
+    clientName: "Sultan",
+    balance: 0,
+    availableTickets: ["d"],
+  ));
+}
+
+class myApp extends StatelessWidget {
+  final String clientName;
+  final double balance;
+  final List<String> availableTickets;
+
+  myApp({
+    required this.clientName,
+    required this.balance,
+    required this.availableTickets,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home Page',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome, $clientName!"),
+            backgroundColor: Color.fromARGB(255, 6, 179, 107),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                      margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                      height: 200,
+                      width: 340,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image:
+                                AssetImage("assets/images/greenbankcard.png")),
+                      ),
+                      child: Center(
+                        child: Text("your balance is"),
+                      )),
+                ),
+                SizedBox(height: 20.0),
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                    height: 400,
+                    width: 680,
+                    // temporary borders
+                    color: Colors.blue,
+                    child: Column(
+                      children: const [Text("Available Tickets:")],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: GNav(
+            backgroundColor: Colors.green,
+            rippleColor: Colors.green, // tab button ripple color when pressed
+            haptic: true, // haptic feedback
+            tabBorderRadius: 15,
+            tabActiveBorder:
+                Border.all(color: Colors.green, width: 1), // tab button border
+            tabBorder:
+                Border.all(color: Colors.green, width: 1), // tab button border
+            tabShadow: [
+              BoxShadow(color: Colors.green.withOpacity(0.5), blurRadius: 8)
+            ], // tab button shadow
+            curve: Curves.easeOutExpo, // tab animation curves
+            duration: Duration(milliseconds: 900), // tab animation duration
+            gap: 8, // the tab button gap between icon and text
+            color: Colors.grey[800], // unselected icon color
+            activeColor: Colors.white, // selected icon and text color
+            iconSize: 24, // tab button icon size
+            tabBackgroundColor:
+                Colors.white.withOpacity(0.1), // selected tab background color
+            padding: EdgeInsets.symmetric(
+                horizontal: 20, vertical: 20), // navigation bar padding
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.train,
+                text: 'Book',
+              ),
+              GButton(
+                icon: Icons.map,
+                text: 'Map',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'settings',
+              )
+            ],
+          )),
+    );
+  }
+}
