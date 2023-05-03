6467ca971197ea245f626a64cc862380d1e8b334
Sul <76082727+Sv6@users.noreply.github.com>
2023-05-03 21:46:38 +0300

    wallet update

--- lib/book.dart
+++ lib/book.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(myApp(clientName: "clientName", balance: 0, destinations: ["destination", "jioadsfjo"], selectedDestination: "destination", availableTickets: ["d"], selectedTicket: "d"));
+  runApp(myApp(
+      clientName: "clientName",
+      balance: 0,
+      destinations: ["destination", "jioadsfjo"],
+      selectedDestination: "destination",
+      availableTickets: ["d"],
+      selectedTicket: "d"));
 }
 
 class myApp extends StatelessWidget {
@@ -20,8 +27,6 @@ class myApp extends StatelessWidget {
     required this.selectedTicket,
   });
 
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,130 +37,216 @@ class myApp extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 6, 179, 107),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Text(
-                        "Balance:",
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                  height: 200,
+                  width: 340,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 6, 179, 107),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SafeArea(
+                            child: Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Text(
+                                "$clientName",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SafeArea(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "#1233",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  // fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "SAR$balance",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Pass Counter: 0",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SafeArea(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: FloatingActionButton.small(
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.green,
+                              onPressed: () {},
+                              child: Icon(Icons.add)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Balance:",
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "\$${balance.toStringAsFixed(2)}",
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "From:",
+                                  style: TextStyle(fontSize: 18.0),
+                                ),
+                                DropdownButton<String>(
+                                  value: selectedDestination,
+                                  onChanged: (String? value) {},
+                                  items: destinations
+                                      .map((destination) =>
+                                      DropdownMenuItem<String>(
+                                        value: destination,
+                                        child: Text(destination),
+                                      ))
+                                      .toList(),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "To:",
+                                  style: TextStyle(fontSize: 18.0),
+                                ),
+                                DropdownButton<String>(
+                                  value: selectedDestination,
+                                  onChanged: (String? value) {},
+                                  items: destinations
+                                      .map((destination) =>
+                                      DropdownMenuItem<String>(
+                                        value: destination,
+                                        child: Text(destination),
+                                      ))
+                                      .toList(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        "Available tickets:",
                         style: TextStyle(fontSize: 18.0),
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        "\$${balance.toStringAsFixed(2)}",
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      availableTickets.isEmpty
+                          ? Text(
+                        "You have no tickets.",
+                        style: TextStyle(fontSize: 16.0),
+                      )
+                          : Column(
+                        children: availableTickets
+                            .map((ticket) => InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 8.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                              ),
+                              borderRadius: BorderRadius.circular(
+                                8.0,
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(ticket),
+                                Icon(Icons.arrow_forward),
+                              ],
+                            ),
+                          ),
+                        ))
+                            .toList(),
                       ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "From:",
-                                style: TextStyle(fontSize: 18.0),
-                              ),
-                              DropdownButton<String>(
-                                value: selectedDestination,
-                                onChanged: (String? value) {},
-                                items: destinations
-                                    .map(
-                                        (destination) => DropdownMenuItem<String>(
-                                      value: destination,
-                                      child: Text(destination),
-                                    ))
-                                    .toList(),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "To:",
-                                style: TextStyle(fontSize: 18.0),
-                              ),
-                              DropdownButton<String>(
-                                value: selectedDestination,
-                                onChanged: (String? value) {},
-                                items: destinations
-                                    .map(
-                                        (destination) => DropdownMenuItem<String>(
-                                      value: destination,
-                                      child: Text(destination),
-                                    ))
-                                    .toList(),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      "Available tickets:",
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                    availableTickets.isEmpty
-                        ? Text(
-                      "You have no tickets.",
-                      style: TextStyle(fontSize: 16.0),
-                    )
-                        : Column(
-                      children: availableTickets
-                          .map((ticket) => InkWell(
+                      SizedBox(height: 16.0),
+                      InkWell(
                         onTap: () {},
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 8.0),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.grey,
-                            ),
-                            borderRadius: BorderRadius.circular(
-                              8.0,
-                            ),
+                        child: Text(
+                          "Book now",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.blue,
                           ),
-                          child: Row(
-                            mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(ticket),
-                              Icon(Icons.arrow_forward),
-                            ],
-                          ),
-                        ),
-                      ))
-                          .toList(),
-                    ),
-                    SizedBox(height: 16.0),
-                    InkWell(
-                      onTap: () {},
-                      child: Text(
-                        "Book now",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.blue,
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -182,4 +273,3 @@ class myApp extends StatelessWidget {
     );
   }
 }
-//jnjnijnjnkk
