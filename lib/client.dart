753250c02efea8db1d0b93850896fe58854aeaf2
Sul <76082727+Sv6@users.noreply.github.com>
2023-05-03 21:50:06 +0300

    wallet update

--- lib/client.dart
+++ lib/client.dart
@@ -25,159 +25,159 @@ class myApp extends StatelessWidget {
     return MaterialApp(
       title: 'Home Page',
       home: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 50,
-            title: Text("Welcome, $clientName!"),
-            backgroundColor: Color.fromARGB(255, 6, 179, 107),
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Center(
-                  child: SafeArea(
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                      height: 200,
-                      width: 340,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 6, 179, 107),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SafeArea(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15),
-                                  child: Text(
-                                    "$clientName",
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SafeArea(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "#1233",
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      // fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                "SAR$balance",
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                "Pass Counter: 0",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SafeArea(
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: FloatingActionButton.small(
-                                  backgroundColor: Colors.white,
-                                  foregroundColor: Colors.green,
-                                  onPressed: () {},
-                                  child: Icon(Icons.add)),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Center(
+        appBar: AppBar(
+          toolbarHeight: 50,
+          title: Text("Welcome, $clientName!"),
+          backgroundColor: Color.fromARGB(255, 6, 179, 107),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: SafeArea(
                   child: Container(
                     margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                    height: 400,
-                    width: 680,
-                    // temporary borders
-                    color: Colors.blue,
+                    height: 200,
+                    width: 340,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 6, 179, 107),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
                     child: Column(
-                      children: const [Text("Available Tickets:")],
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SafeArea(
+                              child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Text(
+                                  "$clientName",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SafeArea(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  "#1233",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    // fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "SAR$balance",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Pass Counter: 0",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SafeArea(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: FloatingActionButton.small(
+                                backgroundColor: Colors.white,
+                                foregroundColor: Colors.green,
+                                onPressed: () {},
+                                child: Icon(Icons.add)),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
-          ),
-          bottomNavigationBar: GNav(
-            backgroundColor: Colors.green,
-            rippleColor: Colors.green, // tab button ripple color when pressed
-            haptic: true, // haptic feedback
-            tabBorderRadius: 15,
-            tabActiveBorder:
-                Border.all(color: Colors.green, width: 1), // tab button border
-            tabBorder:
-                Border.all(color: Colors.green, width: 1), // tab button border
-            tabShadow: [
-              BoxShadow(color: Colors.green.withOpacity(0.5), blurRadius: 8)
-            ], // tab button shadow
-            curve: Curves.easeOutExpo, // tab animation curves
-            duration: Duration(milliseconds: 900), // tab animation duration
-            gap: 8, // the tab button gap between icon and text
-            color: Colors.grey[800], // unselected icon color
-            activeColor: Colors.white, // selected icon and text color
-            iconSize: 24, // tab button icon size
-            tabBackgroundColor:
-                Colors.white.withOpacity(0.1), // selected tab background color
-            padding: EdgeInsets.symmetric(
-                horizontal: 20, vertical: 20), // navigation bar padding
-            tabs: const [
-              GButton(
-                icon: Icons.home,
-                text: 'Home',
               ),
-              GButton(
-                icon: Icons.train,
-                text: 'Book',
+              SizedBox(height: 20.0),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                  height: 400,
+                  width: 680,
+                  // temporary borders
+                  color: Colors.blue,
+                  child: Column(
+                    children: const [Text("Available Tickets:")],
+                  ),
+                ),
               ),
-              GButton(
-                icon: Icons.map,
-                text: 'Map',
-              ),
-              GButton(
-                icon: Icons.settings,
-                text: 'Settings',
-              )
             ],
           ),
+        ),
+        bottomNavigationBar: GNav(
+          backgroundColor: Colors.green,
+          rippleColor: Colors.green, // tab button ripple color when pressed
+          haptic: true, // haptic feedback
+          tabBorderRadius: 15,
+          tabActiveBorder:
+          Border.all(color: Colors.green, width: 1), // tab button border
+          tabBorder:
+          Border.all(color: Colors.green, width: 1), // tab button border
+          tabShadow: [
+            BoxShadow(color: Colors.green.withOpacity(0.5), blurRadius: 8)
+          ], // tab button shadow
+          curve: Curves.easeOutExpo, // tab animation curves
+          duration: Duration(milliseconds: 900), // tab animation duration
+          gap: 8, // the tab button gap between icon and text
+          color: Colors.grey[800], // unselected icon color
+          activeColor: Colors.white, // selected icon and text color
+          iconSize: 24, // tab button icon size
+          tabBackgroundColor:
+          Colors.white.withOpacity(0.1), // selected tab background color
+          padding: EdgeInsets.symmetric(
+              horizontal: 20, vertical: 20), // navigation bar padding
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.train,
+              text: 'Book',
+            ),
+            GButton(
+              icon: Icons.map,
+              text: 'Map',
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: 'Settings',
+            )
+          ],
+        ),
       ),
     );
   }
