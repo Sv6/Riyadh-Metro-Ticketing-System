bd665d6f34d90a23bbc79af6aa439d064ebbb451
ab03skar <zxcvkillerr@gmail.com>
2023-05-11 19:00:25 +0300

    map not finished

--- lib/login.dart
+++ lib/login.dart
@@ -178,6 +178,7 @@ class _myAppState extends State<LoginPage> {
                         bool validate = true;
                         final email = _email.text;
                         final password = _password.text;
+
                         try {
                           final userCredential = await FirebaseAuth.instance
                               .signInWithEmailAndPassword(
