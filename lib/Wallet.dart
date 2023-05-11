000f604b65b04d85bddca619c2f359a49793f999
zyad <zyad770@gmail.com>
2023-05-11 18:38:42 +0300

    card edit for Wallet+Client

--- lib/Wallet.dart
+++ lib/Wallet.dart
@@ -44,24 +44,24 @@ class _walletPageState extends State<walletPage> {
           ),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                height: 200,
-                width: 340,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 6, 179, 107),
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SafeArea(
-                          child: Padding(
-                            padding: const EdgeInsets.all(15),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                  height: 200,
+                  width: 340,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 6, 179, 107),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Column(
+                    children: [
+                      SafeArea(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
                             child: Text(
                               "${widget.clientName}",
                               style: TextStyle(
@@ -72,149 +72,151 @@ class _walletPageState extends State<walletPage> {
                             ),
                           ),
                         ),
-                        SafeArea(
+                      ),
+                      SafeArea(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
-                              "#1233",
+                              "#1683817305464129",
                               style: TextStyle(
                                 fontSize: 14,
-                                // fontWeight: FontWeight.bold,
-                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white70,
                               ),
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "SAR${widget.balance}",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Pass Counter: 0",
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Column(children: [
-                Text("Enter the amount you want to add"),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 200.0,
-                          child: Padding(
-                            padding: const EdgeInsets.all(15),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                  enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide(
-                                        width: 1,
-                                        color: Colors.grey,
-                                      ))),
+
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "SAR${widget.balance}",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                        SizedBox(
-                            width: 70,
-                            height: 50,
-                            child: ElevatedButton(
-                                onPressed: () {},
-                                child: Text("Add"),
-                                style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        Color.fromARGB(255, 6, 179, 107),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    )))),
-                      ],
-                    )),
-              ]),
-              Text("Choose Your Pass Plan"),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Color.fromARGB(255, 6, 179, 107),
-                    ),
-                    height: 250,
-                    width: 100,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text("10",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                    ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Pass Counter: 0",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Color.fromARGB(255, 6, 179, 107),
+                ),
+                Column(children: [
+                  Text("Enter the amount you want to add"),
+                  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 200.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: BorderSide(
+                                          width: 1,
+                                          color: Colors.grey,
+                                        ))),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              width: 70,
+                              height: 50,
+                              child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("Add"),
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor:
+                                          Color.fromARGB(255, 6, 179, 107),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      )))),
+                        ],
+                      )),
+                ]),
+                Text("Choose Your Pass Plan"),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        color: Color.fromARGB(255, 6, 179, 107),
+                      ),
+                      height: 250,
+                      width: 100,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text("10",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                      ),
                     ),
-                    height: 250,
-                    width: 100,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text("20",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        color: Color.fromARGB(255, 6, 179, 107),
+                      ),
+                      height: 250,
+                      width: 100,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text("20",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                      ),
                     ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Color.fromARGB(255, 6, 179, 107),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        color: Color.fromARGB(255, 6, 179, 107),
+                      ),
+                      height: 250,
+                      width: 100,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text("50",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                      ),
                     ),
-                    height: 250,
-                    width: 100,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text("50",
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                    ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
